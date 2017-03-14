79f04a23ac0864f2e5e31f3cf7bbf27c7b5c02f2
Jesse Furukawa <tuc64377@temple.edu>
2017-03-14 13:43:17 -0700

    initial commit for navbar components

---
+++ frontend/components/navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link, withRouter } from 'react-router';
+
+class NavBar extends React.Component {
+  constructor(props){
+    super(props);
+
+  }
+
+  render(){
+    
+  }
+}
+
+export default NavBar;
---
+++ frontend/components/navbar_container.jsx
@@ -0,0 +1,16 @@
+import { connect } from 'react-redux';
+import { signup, login } from '../../actions/session_actions';
+import NavBar from './navbar';
+
+const mapStateToProps = (state, ownProps) => ({
+
+});
+
+const mapDispatchToProps = (dispatch, ownProps) => ({
+
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(NavBar);
