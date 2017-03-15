63106c1989c72c6bf32a4eb07a66a6f3b7fbe930
Jesse Furukawa <tuc64377@temple.edu>
2017-03-14 17:32:45 -0700

    add seeds for cards, decks and subjects

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,13 +8,70 @@
 
 #Create 20 users
 user_arr = []
-20.times do |num|
-  name = Faker::HarryPotter.unique.character.split(" ") #=> "Harry Potter"
+10.times do |num|
+  name = Faker::HarryPotter.unique.character.split(" ")
   username = name.first[0] + name.last
   user_arr << User.create!(username: username, password: "wedemboyz")
 end
 
+#Create Subjects
+subject1 = Subject.create!(title: 'Pokemon', creator_id: user_arr.last.id)
+subject2 = Subject.create!(title: 'Harry Potter', creator_id: user_arr.first.id)
+subject3 = Subject.create!(title: 'Game of Thrones', creator_id: user_arr[1].id)
+
+#Create 4 pokemon decks of 30 cards each under Pokemon subject
+pokemonRegions = %w(kanto johto hoenn sinnoh)
+
 decks = []
-user_arr.each do |user|
-  decks << Deck.create!(author_id: user.id,)
+pokemonRegions.each_with_index do |region, idx|
+  decks << Deck.create!(author_id: user_arr[idx].id,
+                        subject_id: subject1.id,
+                        title: region)
+end
+
+decks.each do |deck|
+  30.times do |num|
+    name = Faker::Pokemon.unique.name
+    location = Faker::Pokemon.location
+    Card.create!(deck_id: deck.id,
+                 question: "Where can #{name} be found?",
+                 answer: "#{location}")
+  end
+  Faker::Pokemon.unique.clear
+end
+
+#Create 1 Game of Thrones deck with 20 cards
+deck = Deck.create!(author_id: user_arr[1].id,
+                    subject_id: subject3.id,
+                    title: 'Houses')
+20.times do |num|
+  name = Faker::GameOfThrones.unique.character
+  house = Faker::GameOfThrones.house
+  Card.create!(deck_id: deck.id,
+               question: "What house is #{name} in?",
+               answer: "#{house}")
+end
+
+#Create 7 Harry Potter decks with 15 cards each
+books = []
+7.times do
+  books << Faker::HarryPotter.unique.book
+end
+
+decks = []
+books.each_with_index do |book, idx|
+  decks << Deck.create!(author_id: user_arr.sample.id,
+                        subject_id: subject2.id,
+                        title: book)
+end
+
+decks.each do |deck|
+  15.times do |num|
+    character = Faker::HarryPotter.character
+    quote = Faker::HarryPotter.unique.quote
+    Card.create!(deck_id: deck.id,
+                 question: "Who said: \n #{quote}?",
+                 answer: "#{character}")
+  end
+  Faker::HarryPotter.unique.clear
 end
