a8a8cf5846c36ff93e392f7d21b55f8f289fa8ca
Jesse Furukawa <tuc64377@temple.edu>
2017-03-14 23:44:04 -0700

    add card routes and jbuilder views

--- app/controllers/api/cards_controller.rb
+++ app/controllers/api/cards_controller.rb
@@ -1,9 +1,5 @@
 class Api::CardsController < ApplicationController
 
-  def index
-    @cards = Card.all
-  end
-
   def show
     @card = Card.find(params[:id])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     resources :users, only: [:create]
     resource :session, only: [:create, :destroy]
+    resources :cards, only: [:show, :create, :update, :destroy]
+    resources :decks, only: [:index, :show, :create, :destroy]
   end
 
   root "static_pages#root"
