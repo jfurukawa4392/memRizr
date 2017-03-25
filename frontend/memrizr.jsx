acec606fd598aea72a64638c6af00d287ed0327f
Jesse Furukawa <tuc64377@temple.edu>
2017-03-25 11:44:53 -0700

    take store and other actions off the window

--- frontend/memrizr.jsx
+++ frontend/memrizr.jsx
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
 import Modal from 'react-modal';
-import * as SubjectActions from './actions/subject_actions';
-import * as DeckActions from './actions/deck_actions';
-import * as SearchActions from './actions/search_actions';
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById('root');
@@ -17,11 +14,6 @@ document.addEventListener("DOMContentLoaded", () => {
     store = configureStore();
   }
   Modal.setAppElement(root);
-  window.store = store;
-  window.state = store.getState;
-  window.requestResults = SearchActions.fetchResults;
-  window.createFollow = SubjectActions.createFollow;
-  window.deleteFollow = SubjectActions.deleteFollow;
 
   ReactDOM.render(<Root store={store}/>, root);
 });
